009ee08de1e6963b4f77d270804a8631e0ea6a5f
Suhas Vishwanath <vishwas@thoughtworks.com>
2015-12-14 22:54:53 +0530

    Suhas | Solutions to day 12 and 13

--- day-12.rb
+++ day-12.rb
@@ -1,6 +1,29 @@
+require 'json'
+
 filename = ARGV[0] || "day-12-input.txt"
+
+class Hash
+	def clean(key)
+		return "" if self.values.include?(key)
+		self.keys.each do |k|
+			v = self[k]
+			puts "Hash\#clean #{v.class}"
+			self[k] = (v.is_a?(Hash) || v.is_a?(Array)) ? v.clean(key) : v
+		end
+		return self
+	end
+end
+
+class Array
+	def clean(key)
+		self.map{|element| puts "Array\#clean #{element.class}"; (element.is_a?(Hash) || element.is_a?(Array))  ? element.clean(key) : element }
+	end
+end
+
 File.readlines(filename).each { |line|
-	cleaned_line = line.gsub(/{[^{]*(?>"red":|:"red")[^}]*}/, "")
 	puts line.scan(/-?\d+/).map(&:to_i).inject(0){|a,v| a+v}
-	puts cleaned_line.scan(/-?\d+/).map(&:to_i).inject(0){|a,v| a+v}
+	data = JSON.parse(line)
+	new_line = JSON.generate(data.clean("red"))
+	puts new_line
+	puts new_line.scan(/-?\d+/).map(&:to_i).inject(0){|a,v| a+v}
 }
